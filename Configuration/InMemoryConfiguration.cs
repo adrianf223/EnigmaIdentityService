3a7cda466c07c17ba2f26cbe6fd259fec5f2ad3d
Adrian <adrianf223@gmail.com>
2019-04-25 15:13:33 +0300

    init ref proj

---
+++ Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,71 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enigma.OAuth.Configuration
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[] {
+                new ApiResource("Enigma", "Enigma Resource")
+            };
+        }
+
+
+        public static IEnumerable<IdentityResource> IdentityResources()
+        {
+            return new IdentityResource[] {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[] {
+                new Client
+                {
+                    ClientId = "Enigma",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "Enigma" }
+                },
+
+                new Client
+                {
+                    ClientId = "Enigma_code",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    AllowedScopes = new [] {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "Enigma"
+                    },
+                    AllowOfflineAccess = true,
+                    AccessTokenLifetime = 60,
+                    AllowAccessTokensViaBrowser = true,
+                    RedirectUris = new [] { "http://localhost:28849/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:28849/signout-callback-oidc" },
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[] {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "user@mail.com",
+                    Password = "password"
+                }
+            };
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Enigma.OAuth.Configuration;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Enigma.OAuth
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddSigningCredential(new X509Certificate2(@"../localdevcert.pfx", "password"))
+                .AddTestUsers(InMemoryConfiguration.Users().ToList())
+                .AddInMemoryClients(InMemoryConfiguration.Clients())
+                .AddInMemoryIdentityResources(InMemoryConfiguration.IdentityResources())
+                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources());
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
